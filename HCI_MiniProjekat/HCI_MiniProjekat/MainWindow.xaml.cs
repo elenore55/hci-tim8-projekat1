557ed07ddbfb8f9e487a3f2fe6f46cdb884fa352
elenore55 <milica.popovic55@hotmail.com>
2022-04-21 15:56:24 +0200

    Iscrtavanje koordinatnog sistema

--- HCI_MiniProjekat/HCI_MiniProjekat/MainWindow.xaml.cs
+++ HCI_MiniProjekat/HCI_MiniProjekat/MainWindow.xaml.cs
@@ -1,7 +1,14 @@
-﻿using Microsoft.VisualBasic.FileIO;
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Painting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.VisualBasic.FileIO;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using LiveCharts;
+using System.Net;
+using System.Text.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +20,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveChartsCore.SkiaSharpView;
 
 namespace HCI_MiniProjekat
 {
@@ -24,6 +32,10 @@ namespace HCI_MiniProjekat
         public List<string> FromCurrecies { get; set; } = new List<string>();
         public string ToCurrency { get; set; } = "";
         public List<string> Currencies { get; set; }
+
+        public List<ISeries> Series { get; set; } = new List<ISeries>();
+        public List<ISeries> SeriesLine { get; set; } = new List<ISeries>();
+
         public MainWindow()
         {
             LoadCurrencies();
@@ -33,6 +45,58 @@ namespace HCI_MiniProjekat
             tb.ItemsSource = Currencies;
             tb2.ItemsSource = Currencies;
         }
+
+        public List<LiveChartsCore.SkiaSharpView.Axis> XAxes { get; set; }
+            = new List<LiveChartsCore.SkiaSharpView.Axis>
+            {
+                new LiveChartsCore.SkiaSharpView.Axis
+                {
+                    Name = "My X Axis",
+                    NamePaint = new SolidColorPaint(SKColors.Black),
+                    Labels = new List<string>(),
+                    LabelsPaint = new SolidColorPaint(SKColors.Blue),
+                    TextSize = 8,
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray) { StrokeThickness = 2 }
+                }
+            };
+
+        public List<LiveChartsCore.SkiaSharpView.Axis> YAxes { get; set; }
+            = new List<LiveChartsCore.SkiaSharpView.Axis>
+            {
+                new LiveChartsCore.SkiaSharpView.Axis
+                {
+                    Name = "My Y Axis",
+                    TextSize = 10,
+                    LabelsPaint = new SolidColorPaint(SKColors.Black),
+                    // Labeler = Labelers.Default
+                }
+            };
+
+        public List<LiveChartsCore.SkiaSharpView.Axis> XAxesLine { get; set; }
+            = new List<LiveChartsCore.SkiaSharpView.Axis>
+            {
+                new LiveChartsCore.SkiaSharpView.Axis
+                {
+                    Name = "My X Axis",
+                    NamePaint = new SolidColorPaint(SKColors.Black),
+                    Labels = new List<string>(),
+                    LabelsPaint = new SolidColorPaint(SKColors.Blue),
+                    TextSize = 8,
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray) { StrokeThickness = 2 }
+                }
+            };
+
+        public List<LiveChartsCore.SkiaSharpView.Axis> YAxesLine { get; set; }
+            = new List<LiveChartsCore.SkiaSharpView.Axis>
+            {
+                new LiveChartsCore.SkiaSharpView.Axis
+                {
+                    Name = "My Y Axis",
+                    TextSize = 10,
+                    LabelsPaint = new SolidColorPaint(SKColors.Black),
+                    // Labeler = Labelers.Default
+                }
+            };
 
         private void LoadCurrencies()
         {
@@ -146,7 +210,92 @@ namespace HCI_MiniProjekat
                     MessageBox.Show("Datumi moraju biti izabrani hronoloski!");
                 }
                 MessageBox.Show("Izabarali ste sve opcije!");
+                DisplayChart();
             }
+        }
+
+        private void DisplayChart()
+        {
+            // imamo FromCurrencies
+            // imamo ToCurrency
+            // treba function, npr. FX_MONTH
+            // FromDate.SelectedDate.GetValueOrDefault i ToDate.SelectedDate.GetValueOrDefault() - start i end date
+            // Type.SelectedItem - open, close, high, low
+            // Interval.SelectedItem - funkcija
+
+            DateTime startDate = FromDate.SelectedDate.GetValueOrDefault();
+            DateTime endDate = ToDate.SelectedDate.GetValueOrDefault();
+
+            foreach (string curr in FromCurrecies)
+            {
+                string queryURL = FormURL(curr.Substring(0, 3));
+                Uri queryUri = new Uri(queryURL);
+
+                using (WebClient client = new WebClient())
+                {
+                    // Time Series FX ({Intertval.Text})
+                    dynamic json_data = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(client.DownloadString(queryUri));
+                    dynamic data = json_data[$"Time Series FX ({Intertval.Text})"];
+                    RouteValueDictionary result = new RouteValueDictionary(data);
+                    ChartValues<double> values = new ChartValues<double>();
+
+
+                    foreach (var key in result.Keys)
+                    {
+                        MessageBox.Show(key);
+                        
+                        DateTime oDate = Convert.ToDateTime(key);
+                        if (DateTime.Compare(oDate, startDate) >= 0 && DateTime.Compare(oDate, endDate) <= 0)
+                        {
+                            XAxes.ElementAt(0).Labels.Add(key);
+                            XAxesLine.ElementAt(0).Labels.Add(key);
+                            object obj = new object();
+                            result.TryGetValue(key, out obj);
+                            RouteValueDictionary d = new RouteValueDictionary(obj);
+                            d.TryGetValue(Type.Text.ToLower(), out obj);
+                            double num = Convert.ToDouble(obj);
+                            values.Add(num);
+                        }
+                    }
+
+                    Series.Add(
+                        new ColumnSeries<double>
+                        {
+                            Values = values,
+                            Name = curr,
+                            GroupPadding = 10
+                        }
+                    );
+
+                    SeriesLine.Add(
+                        new LineSeries<double>
+                        {
+                            Values = values,
+                            Stroke = new SolidColorPaint(SKColors.Red) { StrokeThickness = 2 },
+                            Name = curr,
+                            GeometrySize = 10,
+                            Fill = null
+                        }
+                    );
+
+                    DataContext = this;
+                }
+            }
+        }
+
+        private string FormURL(string from)
+        {
+            // return "https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=USD&apikey=UWN3B1CJC7X8TNGE";
+            string fn = $"FX_{Intertval.Text.ToUpper()}";
+            string to = ToCurrency.Substring(0, 3);
+            if (fn != "FX_INTRADAY")
+                return $"https://www.alphavantage.co/query?function={fn}&from_symbol={from}&to_symbol={to}&apikey=UWN3B1CJC7X8TNGE";
+            return $"https://www.alphavantage.co/query?function={fn}&from_symbol={from}&to_symbol={to}&interval={TimeInterval.Text}min&apikey=UWN3B1CJC7X8TNGE";
+        }
+
+        private void CartesianChart_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Double clicked!");
         }
     }
 }
