17e108eca0184fb73249d0f7b95636212f9d1a0f
Nevena Radešić <radesic.sw12.2019@uns.ac.rs>
2022-04-25 20:54:02 +0200

    Dodati fajlovi za prikaz tabele

---
+++ HCI_MiniProjekat/HCI_MiniProjekat/CurrencyTable.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HCI_MiniProjekat
+{
+    /// <summary>
+    /// Interaction logic for CurrencyTable.xaml
+    /// </summary>
+    public partial class CurrencyTable : Window
+    {
+        public ObservableCollection<TableRow> Rows { get; set; }
+        public string Currency { get; set; }
+        public ObservableCollection<string> FromCurrenciesSymbols { get; set; }
+        public string CurrencyTitle { get; set; }
+        public int selectedIndex { get; set; }
+        public CurrencyTable()
+        {
+            Rows = new ObservableCollection<TableRow>();
+            FromCurrenciesSymbols = new ObservableCollection<string>();
+            InitializeComponent();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            e.Cancel = true;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DisplayTableButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+    }
+}
---
+++ HCI_MiniProjekat/HCI_MiniProjekat/TableRow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI_MiniProjekat
+{
+    public class TableRow
+    {
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Close { get; set; }
+        public string DateString { get; set; }
+
+        public TableRow() { }
+
+        public override string ToString()
+        {
+            return Open + " " + High + " " + Low + " " + Close + " " + DateString;
+
+        }
+    }
+}
